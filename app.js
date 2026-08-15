54ee8031be9de83e33db18c16bf5ec39e4e6a7de
Mus-1323 <dewanimuskan24@gmail.com>
2026-08-15 19:44:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -259,7 +259,7 @@
                  const apiKey = "AQ.Ab8RN6J1clT7caeUxh2jivNWr4CJDWs6sWAgOjRO0GTbR2ebDQ";
 
                  // 1. URL without the ?key= parameter
-                 const apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"; 
+                 const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
                   const payload = {
                   contents: [{ parts: [{ text: userQuery }] }],
